bcfc1b3493d56896cf9ed79d9af9249ee4dffd1e
N0364359 <michael.blydenburgh@libertymutual.com>
2019-04-13 18:41:45 -0400

    Removed UI files to make separate repo. Enable Cross origin to localhost:3000

--- src/main/kotlin/com/mblydenburgh/teatime/controller/Uicontroller.kt
+++ src/main/kotlin/com/mblydenburgh/teatime/controller/Uicontroller.kt
@@ -2,6 +2,7 @@ package com.mblydenburgh.teatime.controller
 
 import com.mblydenburgh.teatime.dto.TeaDto
 import com.mblydenburgh.teatime.service.TeaService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,6 +14,7 @@ class Uicontroller(val teaService: TeaService) {
         return "hello"
     }
 
+    @CrossOrigin(origins = ["http://localhost:3000"])
     @GetMapping("/api/tea")
     fun fetchTeas():List<TeaDto>{
         return teaService.returnTeasFromDatabase()
